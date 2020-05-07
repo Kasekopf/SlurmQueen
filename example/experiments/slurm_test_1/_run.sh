ea77150bbd7483da136cac2e620509a860ce4e98
Kasekopf <jeffreydudek@gmail.com>
2020-05-07 12:46:01 -0700

    Use NOTS for example

--- example/experiments/slurm_test_1/_run.sh
+++ example/experiments/slurm_test_1/_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --time=5:00
 #SBATCH --job-name=slurm_test_1
-#SBATCH --partition=commons
+#SBATCH --partition=scavenge
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 module load Anaconda3/5.0.0
