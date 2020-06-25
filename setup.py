5120af3ecea76c66a0dbd9b91a38a9fff3694dab
Kasekopf <jeffreydudek@gmail.com>
2020-06-24 18:19:07 -0700

    Version 1.6.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="slurmqueen",
-    version="1.6.0",
+    version="1.6.1",
     author="Jeffrey Dudek",
     author_email="jeffreydudek@gmail.com",
     description="A Python 3 library for automatically running experiments using a Slurm cluster",
